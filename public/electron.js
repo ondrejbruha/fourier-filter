a6298a18e3f47b68f50456890003ff4f568fd6ff
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-04-16 12:50:00 +0200

    fix open window on mac

--- public/electron.js
+++ public/electron.js
@@ -29,7 +29,13 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
